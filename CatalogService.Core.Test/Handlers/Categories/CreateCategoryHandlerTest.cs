089c948112ae16eb55ddcde814985a177529d37a
Vladislav Chikhira <Vladislav_Chikhira@epam.com>
2023-06-05 20:39:36 +0300

    Added tests dummies

---
+++ CatalogService.Core.Test/Handlers/Categories/CreateCategoryHandlerTest.cs
@@ -0,0 +1,33 @@
+﻿using CatalogService.Core.Commands;
+using CatalogService.Core.Handlers.Categories;
+using CatalogService.Infrastructure.MsSql.Categories;
+using Moq;
+
+namespace CatalogService.Core.Test.Handlers.Categories;
+
+public class CreateCategoryHandlerTest
+{
+    private const int DummyId = 42;
+    [Fact]
+    public async Task HandleAsync_ReturnsExpected()
+    {
+        // Arrange
+        ICreateCategoryHandler categoryHandler = new CreateCategoryHandler(GetRepositoryMock().Object);
+
+        // Act
+        int result = await categoryHandler.HandleAsync(new CategoryCommand());
+
+        // Assert
+        Assert.Equal(DummyId, result);
+    }
+
+    private static Mock<ICategoryRepository> GetRepositoryMock()
+    {
+        var repository = new Mock<ICategoryRepository>();
+        repository
+            .Setup(x => x.CreateNew(It.IsAny<CreateUpdateCategoryCommand>()))
+            .ReturnsAsync(DummyId);
+
+        return repository;
+    }
+}
